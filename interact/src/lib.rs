cb140f7e96f04d209d23f8e684fc30045ced5ee6
Junha Yang <junhayang1@gmail.com>
2022-07-06 00:53:33 +0900

    Add query utility function

--- interact/src/lib.rs
+++ interact/src/lib.rs
@@ -1,1 +1,20 @@
+use anyhow::{anyhow, Result};
+use reqwest::Client;
+use serde_json::Value;
 
+pub async fn request(client: &Client, url: &str, block_number: Option<u64>) -> Result<Value> {
+    let response = if let Some(block_number) = block_number {
+        client
+            .get(url)
+            .header("x-cosmos-block-height", format!("{}", block_number))
+            .send()
+            .await?
+    } else {
+        client.get(url).send().await?
+    };
+    let text = format!("{:?}", response);
+    response
+        .json::<Value>()
+        .await
+        .map_err(|_| anyhow!("Failed to parse to json: {}", text))
+}
