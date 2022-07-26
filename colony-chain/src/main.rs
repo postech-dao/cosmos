0249f94c3dc5f46efa28402f7a02771a22d60334
Junha Yang <junhayang1@gmail.com>
2022-07-26 18:50:45 +0900

    Add dummy impl. of ColonyChain

---
+++ colony-chain/src/main.rs
@@ -0,0 +1,23 @@
+use pbc_colony_common::*;
+use pdao_cosmos_colony_chain::*;
+use std::sync::Arc;
+
+#[tokio::main]
+async fn main() {
+    let port = std::env::args()
+        .nth(1)
+        .unwrap_or_else(|| "80".to_owned())
+        .parse::<u16>()
+        .unwrap();
+    println!("RUN ON PORT {}", port);
+    serde_tc::http::run_server(
+        port,
+        vec![(
+            "juno".to_owned(),
+            serde_tc::http::create_http_object(Arc::new(Juno {}) as Arc<dyn ColonyChain>),
+        )]
+        .into_iter()
+        .collect(),
+    )
+    .await;
+}
