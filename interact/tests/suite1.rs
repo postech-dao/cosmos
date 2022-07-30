d94df6c6ac8e0707de2d065ab295398592cda151
Geunwoo Kim <posgnu@gmail.com>
2022-07-29 18:38:01 -0700

    <feat>: add test for contract instantiation

--- interact/tests/suite1.rs
+++ interact/tests/suite1.rs
@@ -142,7 +142,7 @@ async fn store_contract() {
     // For paying the gas fee
     let amount = Coin {
         amount: 100000u32.into(),
-        denom: "ujunox".parse().unwrap(),
+        denom: "umlg".parse().unwrap(),
     };
 
     let mut file = File::open("./simple-counter/artifact/simple_counter.wasm").unwrap();
@@ -164,6 +164,69 @@ async fn store_contract() {
     let timeout_height = 0u16;
 
     let tx_body = tx::Body::new(vec![msg], "test memo", timeout_height);
+    let auth_info =
+        SignerInfo::single_direct(Some(sender_public_key), sequence_number).auth_info(fee);
+    // account_number: check at cosmos/auth/v1beta1/accounts/juno123412341234 (your address)
+    let sign_doc = SignDoc::new(&tx_body, &auth_info, &chain_id, 123456).unwrap();
+    let tx_raw = sign_doc.sign(&sender_private_key).unwrap();
+
+    let rpc_address = "https://rpc.malaga-420.cosmwasm.com:443".to_owned();
+    let rpc_client = rpc::HttpClient::new(rpc_address.as_str()).unwrap();
+    let tx_commit_response =
+        rpc::Client::broadcast_tx_commit(&rpc_client, tx_raw.to_bytes().unwrap().into())
+            .await
+            .unwrap();
+
+    // check the response
+    if tx_commit_response.check_tx.code.is_err() {
+        panic!("check_tx failed: {:?}", tx_commit_response.check_tx);
+    }
+
+    if tx_commit_response.deliver_tx.code.is_err() {
+        panic!("deliver_tx failed: {:?}", tx_commit_response.deliver_tx);
+    }
+
+    let tx = dev::poll_for_tx(&rpc_client, tx_commit_response.hash).await;
+    assert_eq!(&tx_body, &tx.body);
+    assert_eq!(&auth_info, &tx.auth_info);
+}
+
+#[ignore]
+#[tokio::test]
+async fn instantiate_contract() {
+    let _config = Config::read_from_env();
+    // Submit a transaction that store the simple-counter contract
+
+    let sender_private_key = secp256k1::SigningKey::random();
+    let sender_public_key = sender_private_key.public_key();
+    let sender_account_id = sender_public_key.account_id("juno").unwrap();
+
+    // For paying the gas fee
+    let amount = Coin {
+        amount: 200000u32.into(),
+        denom: "umlg".parse().unwrap(),
+    };
+
+    let contract_code_id: u64 = 1234;
+
+    let msg_send = cosmrs::cosmwasm::MsgInstantiateContract {
+        sender: sender_account_id.clone(),
+        admin: None,
+        code_id: contract_code_id,
+        label: None,
+        msg: "Instantiate simple counter".as_bytes(),
+        funds: vec![amount.clone()],
+    }
+    .to_any()
+    .unwrap();
+
+    let chain_id = "malaga-420".parse().unwrap();
+    let sequence_number = 0;
+    let gas = 100_0000;
+    let fee = Fee::from_amount_and_gas(amount.clone(), gas);
+    let timeout_height = 0u16;
+
+    let tx_body = tx::Body::new(vec![msg_send], "test memo", timeout_height);
     let auth_info =
         SignerInfo::single_direct(Some(sender_public_key), sequence_number).auth_info(fee);
     // account_number: check at cosmos/auth/v1beta1/accounts/juno123412341234 (your address)
