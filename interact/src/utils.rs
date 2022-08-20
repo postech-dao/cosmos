8cf6055ec94423221ab8e99bf122b97881f3a412
JeongHunP <jhp0618@postech.ac.kr>
2022-08-20 11:16:14 +0900

    feat: change testnet to uni-3 and complete implementing interact tests

--- interact/src/utils.rs
+++ interact/src/utils.rs
@@ -1,4 +1,13 @@
-use cosmrs::crypto;
+use cosmrs::{bip32, crypto};
+
+pub fn mnemonic_to_private_key(mnemonic: String, password: &str) -> crypto::secp256k1::SigningKey {
+    let seed = bip32::Mnemonic::new(mnemonic, bip32::Language::English)
+        .unwrap()
+        .to_seed(password);
+    let private_key: crypto::secp256k1::SigningKey = bip32::XPrv::new(seed).unwrap().into();
+
+    private_key
+}
 
 pub fn private_to_pub_and_account(
     sender_private_key: &crypto::secp256k1::SigningKey,
