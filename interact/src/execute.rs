c8ef3676213ab068e6864941f7ac5cc5f128ba77
JeongHunP <jhp0618@postech.ac.kr>
2022-09-06 16:08:57 +0900

    refractor: error handling

--- interact/src/execute.rs
+++ interact/src/execute.rs
@@ -6,6 +6,7 @@ use cosmrs::{
 
 use super::query::{get_account_number, get_sequence_number};
 use super::utils::private_to_pub_and_account;
+use std::error::Error;
 
 #[allow(clippy::too_many_arguments)]
 pub async fn send_execute(
@@ -21,47 +22,44 @@ pub async fn send_execute(
     gas_amount: u32,
     gas_limit: u64,
     tx_memo: Option<&str>,
-) {
+) -> Result<(), Box<dyn Error>> {
     let (sender_public_key, sender_account_id) =
-        private_to_pub_and_account(sender_private_key, account_id);
+        private_to_pub_and_account(sender_private_key, account_id)?;
 
     let funds = Coin {
         amount: funds.into(),
-        denom: denom.parse().unwrap(),
+        denom: denom.parse()?,
     };
 
     let msg = cosmrs::cosmwasm::MsgExecuteContract {
         sender: sender_account_id.clone(),
-        contract: contract_address.parse().unwrap(),
+        contract: contract_address.parse()?,
         msg: execute_msg,
         funds: vec![funds.clone()],
     }
-    .to_any()
-    .unwrap();
+    .to_any()?;
 
     let amount = Coin {
         amount: gas_amount.into(),
-        denom: denom.parse().unwrap(),
+        denom: denom.parse()?,
     };
 
-    let chain_id = chain_id.parse().unwrap();
-    let sequence_number = get_sequence_number(api_address, sender_account_id.as_ref()).await;
+    let chain_id = chain_id.parse()?;
+    let sequence_number = get_sequence_number(api_address, sender_account_id.as_ref()).await?;
     let fee = Fee::from_amount_and_gas(amount.clone(), gas_limit);
     let timeout_height = 0u16;
 
-    let tx_body = tx::Body::new(vec![msg], tx_memo.unwrap_or("test memo"), timeout_height);
+    let tx_body = tx::Body::new(vec![msg], tx_memo.ok_or("test memo")?, timeout_height);
     let auth_info =
         SignerInfo::single_direct(Some(sender_public_key), sequence_number).auth_info(fee);
-    let account_number = get_account_number(api_address, sender_account_id.as_ref()).await;
-    let sign_doc = SignDoc::new(&tx_body, &auth_info, &chain_id, account_number).unwrap();
-    let tx_raw = sign_doc.sign(sender_private_key).unwrap();
+    let account_number = get_account_number(api_address, sender_account_id.as_ref()).await?;
+    let sign_doc = SignDoc::new(&tx_body, &auth_info, &chain_id, account_number)?;
+    let tx_raw = sign_doc.sign(sender_private_key)?;
 
     let rpc_address = rpc_address.to_owned();
-    let rpc_client = rpc::HttpClient::new(rpc_address.as_str()).unwrap();
+    let rpc_client = rpc::HttpClient::new(rpc_address.as_str())?;
     let tx_commit_response =
-        rpc::Client::broadcast_tx_commit(&rpc_client, tx_raw.to_bytes().unwrap().into())
-            .await
-            .unwrap();
+        rpc::Client::broadcast_tx_commit(&rpc_client, tx_raw.to_bytes()?.into()).await?;
 
     // check the response
     if tx_commit_response.check_tx.code.is_err() {
@@ -79,4 +77,6 @@ pub async fn send_execute(
     let tx = dev::poll_for_tx(&rpc_client, tx_commit_response.hash).await;
     assert_eq!(&tx_body, &tx.body);
     assert_eq!(&auth_info, &tx.auth_info);
+
+    Ok(())
 }
--- interact/src/query.rs
+++ interact/src/query.rs
@@ -1,14 +1,15 @@
 use crate::request;
 use serde_json::Value;
+use std::error::Error;
 
 pub async fn send_query(
     rest_api_endpoint: &str,
     contract_address: &str,
     encode_msg: &str,
-) -> Value {
+) -> Result<Value, Box<dyn Error>> {
     let client = reqwest::Client::new();
 
-    request(
+    let result = request(
         &client,
         &format!(
             "{}/cosmwasm/wasm/v1/contract/{}/smart/{}",
@@ -16,11 +17,15 @@ pub async fn send_query(
         ),
         None,
     )
-    .await
-    .unwrap()
+    .await?;
+
+    Ok(result)
 }
 
-pub async fn get_code_id(rest_api_endpoint: &str, contract_address: &str) -> u64 {
+pub async fn get_code_id(
+    rest_api_endpoint: &str,
+    contract_address: &str,
+) -> Result<u64, Box<dyn Error>> {
     let client = reqwest::Client::new();
 
     let response = request(
@@ -31,17 +36,20 @@ pub async fn get_code_id(rest_api_endpoint: &str, contract_address: &str) -> u64
         ),
         None,
     )
-    .await
-    .unwrap();
+    .await?;
 
-    response["contract_info"]["code_id"]
+    let code_id = response["contract_info"]["code_id"]
         .as_str()
-        .unwrap()
-        .parse::<u64>()
-        .unwrap()
+        .ok_or("Failed to convert code id to &str")?
+        .parse::<u64>()?;
+
+    Ok(code_id)
 }
 
-pub async fn get_sequence_number(rest_api_endpoint: &str, address: &str) -> u64 {
+pub async fn get_sequence_number(
+    rest_api_endpoint: &str,
+    address: &str,
+) -> Result<u64, Box<dyn Error>> {
     let client = reqwest::Client::new();
 
     let response = request(
@@ -52,17 +60,20 @@ pub async fn get_sequence_number(rest_api_endpoint: &str, address: &str) -> u64 
         ),
         None,
     )
-    .await
-    .unwrap();
+    .await?;
 
-    response["account"]["sequence"]
+    let sequence_number = response["account"]["sequence"]
         .as_str()
-        .unwrap()
-        .parse::<u64>()
-        .unwrap()
+        .ok_or("Failed to convert sequence number to &str")?
+        .parse::<u64>()?;
+
+    Ok(sequence_number)
 }
 
-pub async fn get_account_number(rest_api_endpoint: &str, address: &str) -> u64 {
+pub async fn get_account_number(
+    rest_api_endpoint: &str,
+    address: &str,
+) -> Result<u64, Box<dyn Error>> {
     let client = reqwest::Client::new();
 
     let response = request(
@@ -73,12 +84,12 @@ pub async fn get_account_number(rest_api_endpoint: &str, address: &str) -> u64 {
         ),
         None,
     )
-    .await
-    .unwrap();
+    .await?;
 
-    response["account"]["account_number"]
+    let account_number = response["account"]["account_number"]
         .as_str()
-        .unwrap()
-        .parse::<u64>()
-        .unwrap()
+        .ok_or("Failed to convert account number to &str")?
+        .parse::<u64>()?;
+
+    Ok(account_number)
 }
