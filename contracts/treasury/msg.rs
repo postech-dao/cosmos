f5474c905d8c3339ea687f055a9b477dd4258f5c
박채영 <cheyoungpark@bagchaeyeong-ui-MacBook-Pro.local>
2022-10-09 09:30:01 +0900

    added lib, msg & corrected contract

---
+++ contracts/treasury/msg.rs
@@ -0,0 +1,19 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ExecuteMsg {
+    Transfer {
+        recipient: String,
+        amout: Uint128,
+    },
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    GetBalance {
+        token_address: String,
+    },
+}
