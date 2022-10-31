6f3b0ef2afc4fd1de190faa0929826846d5e8fe0
Minsang Seok <minsang5749@naver.com>
2022-10-31 12:57:38 +0900

    Modify Test Contracts for achiving Cross Contract

--- contracts/test-treasury-cross/src/msg.rs
+++ contracts/test-treasury-cross/src/msg.rs
@@ -29,32 +29,61 @@ pub enum QueryMsg {
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
-pub enum VerifyMsg {
-    message: DeliverableMessage,
-    block_height: u64,
-    proof: MerkleProof,
+pub enum QueryMsg {
+    GetBalance {
+        address: String,
+        denom: String,
+    },
+
+    GetAllBalance {
+        address: String,
+    },
 }
 
-impl VerifyMsg {
-    /// serializes the message
-    pub fn into_binary(self) -> StdResult<Binary> {
-        let msg = ExecuteMsg::Verify(self);
-        to_binary(&msg)
-    }
-    /// creates a cosmos_msg sending this struct to the named contract
-    pub fn into_cosmos_msg<
-        T: Into<String>,
-        C
-    >(self, contract_addr: T) -> StdResult<CosmosMsg<C>>
-    where
-    C: Clone + std::fmt::Debug + PartialEq + JsonSchema,
-    {
-        let msg = self.into_binary()?;
-        let execute = WasmMsg::Execute {
-        contract_addr: contract_addr.into(),
-        msg,
-        funds: vec![],
-        };
-        Ok(execute.into())
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum VerifyQueryMsg {
+    CheckVerify {
+        message: DeliverableMessage,
+        block_height: u64,
+        proof: MerkleProof,
     }
 }
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+#[serde(rename_all = "snake_case")]
+pub struct VerifyResponse {
+   pub is_verified: bool,
+}
+
+// #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+// #[serde(rename_all = "snake_case")]
+// pub enum VerifyMsg {
+//     message: DeliverableMessage,
+//     block_height: u64,
+//     proof: MerkleProof,
+// }
+
+// impl VerifyMsg {
+//     /// serializes the message
+//     pub fn into_binary(self) -> StdResult<Binary> {
+//         let msg = ExecuteMsg::Verify(self);
+//         to_binary(&msg)
+//     }
+//     /// creates a cosmos_msg sending this struct to the named contract
+//     pub fn into_cosmos_msg<
+//         T: Into<String>,
+//         C
+//     >(self, contract_addr: T) -> StdResult<CosmosMsg<C>>
+//     where
+//     C: Clone + std::fmt::Debug + PartialEq + JsonSchema,
+//     {
+//         let msg = self.into_binary()?;
+//         let execute = WasmMsg::Execute {
+//         contract_addr: contract_addr.into(),
+//         msg,
+//         funds: vec![],
+//         };
+//         Ok(execute.into())
+//     }
+// }
