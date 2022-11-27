b929cc4995f5bbd78cc1be44282c9496f8830ae3
minsang <minsang5749@naver.com>
2022-11-28 01:35:58 +0900

    Adjust rustfmt

--- contracts/treasury/src/contract.rs
+++ contracts/treasury/src/contract.rs
@@ -1,6 +1,9 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult, CosmosMsg, BankMsg, Uint128, coins};
+use cosmwasm_std::{
+    coins, to_binary, BankMsg, Binary, CosmosMsg, Deps, DepsMut, Env, MessageInfo, Response,
+    StdResult, Uint128,
+};
 use cw2::set_contract_version;
 use pdao_beacon_chain_common::message::DeliverableMessage;
 
@@ -38,8 +41,27 @@ pub fn execute(
     msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
     match msg {
-        ExecuteMsg::LightClientUpdate { header, proof } => execute_light_client_update(deps, _env, info, header, proof),
-        ExecuteMsg::Transfer {recipient, amount, denom, message, block_height, proof} => execute_transfer(deps, _env, info, recipient, amount, denom, message, block_height, proof),
+        ExecuteMsg::LightClientUpdate { header, proof } => {
+            execute_light_client_update(deps, _env, info, header, proof)
+        }
+        ExecuteMsg::Transfer {
+            recipient,
+            amount,
+            denom,
+            message,
+            block_height,
+            proof,
+        } => execute_transfer(
+            deps,
+            _env,
+            info,
+            recipient,
+            amount,
+            denom,
+            message,
+            block_height,
+            proof,
+        ),
     }
 }
 
@@ -71,8 +93,7 @@ fn execute_transfer(
     message: DeliverableMessage,
     block_height: u64,
     proof: String,
-)  -> Result<Response, ContractError> {
-
+) -> Result<Response, ContractError> {
     if amount == Uint128::zero() {
         return Err(ContractError::InvalidZeroAmount {});
     }
@@ -88,39 +109,37 @@ fn execute_transfer(
         } else {
             return Err(ContractError::VerifyFail {});
         }*/
-        if proof == String::from("success"){
+        if proof == String::from("success") {
             Ok(state)
         } else {
             return Err(ContractError::VerifyFail {});
         }
     });
-    
-    if proof == String::from("success"){
-        msgs.push(CosmosMsg::Bank(BankMsg::Send{
+
+    if proof == String::from("success") {
+        msgs.push(CosmosMsg::Bank(BankMsg::Send {
             to_address: recipient,
-            amount: coins(
-                amount_int, 
-                denom,
-            ),
+            amount: coins(amount_int, denom),
         }));
-    
-        Ok(
-            Response::new()
+
+        Ok(Response::new()
             .add_attribute("method", "execute_transfer")
-            .add_messages(msgs)
-        )
+            .add_messages(msgs))
     } else {
         return Err(ContractError::VerifyFail {});
     }
-    
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
     match msg {
         QueryMsg::GetHeader {} => to_binary(&query_header(deps)?),
-        QueryMsg::GetBalance {denom} => to_binary(&deps.querier.query_balance(_env.contract.address, denom)?),
-        QueryMsg::GetAllBalances {} => to_binary(&deps.querier.query_all_balances(_env.contract.address)?),
+        QueryMsg::GetBalance { denom } => {
+            to_binary(&deps.querier.query_balance(_env.contract.address, denom)?)
+        }
+        QueryMsg::GetAllBalances {} => {
+            to_binary(&deps.querier.query_all_balances(_env.contract.address)?)
+        }
     }
 }
 
@@ -135,18 +154,22 @@ fn query_header(deps: Deps) -> StdResult<GetHeaderResponse> {
 mod test {
     use super::*;
     use cosmwasm_std::testing::{mock_dependencies_with_balance, mock_env, mock_info};
-    use cosmwasm_std::{from_binary, Coin, coin, SubMsg, ReplyOn};
+    use cosmwasm_std::{coin, from_binary, Coin, ReplyOn, SubMsg};
     use pdao_beacon_chain_common::message::FungibleTokenTransfer;
 
     #[test]
-    fn query_header_test(){
-        let mut deps = mock_dependencies_with_balance(&vec![coin(123, "gold"), coin(456, "silver"), coin(789, "bronze")]);
+    fn query_header_test() {
+        let mut deps = mock_dependencies_with_balance(&vec![
+            coin(123, "gold"),
+            coin(456, "silver"),
+            coin(789, "bronze"),
+        ]);
         let chain_name = String::from("chain name");
         let header = String::from("abc");
         let env = mock_env();
 
-        let info = mock_info("sender", &coins(2,"token"));
-        let msg = InstantiateMsg{header, chain_name};
+        let info = mock_info("sender", &coins(2, "token"));
+        let msg = InstantiateMsg { header, chain_name };
         let _res = instantiate(deps.as_mut(), env, info, msg);
 
         let res = query(deps.as_ref(), mock_env(), QueryMsg::GetHeader {}).unwrap();
@@ -155,46 +178,54 @@ mod test {
     }
 
     #[test]
-    fn query_balance_test(){
-        let mut deps = mock_dependencies_with_balance(&vec![coin(123, "gold"), coin(456, "silver"), coin(789, "bronze")]);
+    fn query_balance_test() {
+        let mut deps = mock_dependencies_with_balance(&vec![
+            coin(123, "gold"),
+            coin(456, "silver"),
+            coin(789, "bronze"),
+        ]);
         let chain_name = String::from("chain name");
         let header = String::from("abc");
         let env = mock_env();
 
-        let info = mock_info("sender", &coins(2,"token"));
-        let msg = InstantiateMsg{header, chain_name};
+        let info = mock_info("sender", &coins(2, "token"));
+        let msg = InstantiateMsg { header, chain_name };
         let _res = instantiate(deps.as_mut(), env, info, msg);
 
         let denom = String::from("gold");
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetBalance {denom}).unwrap();
+        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetBalance { denom }).unwrap();
         let value: Coin = from_binary(&res).unwrap();
         assert_eq!(123, value.amount.u128());
 
         let denom = String::from("silver");
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetBalance {denom}).unwrap();
+        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetBalance { denom }).unwrap();
         let value: Coin = from_binary(&res).unwrap();
         assert_eq!(456, value.amount.u128());
 
         let denom = String::from("bronze");
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetBalance {denom}).unwrap();
+        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetBalance { denom }).unwrap();
         let value: Coin = from_binary(&res).unwrap();
         assert_eq!(789, value.amount.u128());
     }
 
     #[test]
-    fn query_all_balances_test(){
-        let mut deps = mock_dependencies_with_balance(&vec![coin(123, "gold"), coin(456, "silver"), coin(789, "bronze")]);
+    fn query_all_balances_test() {
+        let mut deps = mock_dependencies_with_balance(&vec![
+            coin(123, "gold"),
+            coin(456, "silver"),
+            coin(789, "bronze"),
+        ]);
         let chain_name = String::from("chain name");
         let header = String::from("abc");
         let env = mock_env();
-        
-        let info = mock_info("sender", &coins(2,"token"));
-        let msg = InstantiateMsg{header, chain_name};
+
+        let info = mock_info("sender", &coins(2, "token"));
+        let msg = InstantiateMsg { header, chain_name };
         let _res = instantiate(deps.as_mut(), env, info, msg);
 
         let res = query(deps.as_ref(), mock_env(), QueryMsg::GetAllBalances {}).unwrap();
         let value: Vec<Coin> = from_binary(&res).unwrap();
-    
+
         assert_eq!(coin(123, "gold"), value[0]);
         assert_eq!(123, value[0].amount.u128());
 
@@ -206,14 +237,18 @@ mod test {
     }
 
     #[test]
-    fn transfer_test(){
-        let mut deps = mock_dependencies_with_balance(&vec![coin(123, "gold"), coin(456, "silver"), coin(789, "bronze")]);
+    fn transfer_test() {
+        let mut deps = mock_dependencies_with_balance(&vec![
+            coin(123, "gold"),
+            coin(456, "silver"),
+            coin(789, "bronze"),
+        ]);
         let chain_name = String::from("chain name");
         let header = String::from("abc");
         let env = mock_env();
 
-        let info = mock_info("sender", &coins(2,"token"));
-        let msg = InstantiateMsg{header, chain_name};
+        let info = mock_info("sender", &coins(2, "token"));
+        let msg = InstantiateMsg { header, chain_name };
         let _res = instantiate(deps.as_mut(), env, info, msg);
         let ftt = FungibleTokenTransfer {
             token_id: String::from("gold"),
@@ -222,33 +257,46 @@ mod test {
             contract_sequence: u64::from(1u64),
         };
 
-        let msg = ExecuteMsg::Transfer { recipient: String::from("recipient"), amount: Uint128::from(10u128), denom: String::from("gold"), message: DeliverableMessage::FungibleTokenTransfer(ftt), block_height: u64::from(10u64), proof: String::from("success") };
-        let sub = SubMsg{
+        let msg = ExecuteMsg::Transfer {
+            recipient: String::from("recipient"),
+            amount: Uint128::from(10u128),
+            denom: String::from("gold"),
+            message: DeliverableMessage::FungibleTokenTransfer(ftt),
+            block_height: u64::from(10u64),
+            proof: String::from("success"),
+        };
+        let sub = SubMsg {
             id: 0u64,
-            msg: CosmosMsg::Bank(BankMsg::Send{
+            msg: CosmosMsg::Bank(BankMsg::Send {
                 to_address: String::from("recipient"),
-                amount: coins(
-                    10, 
-                    "gold",
-                ),
+                amount: coins(10, "gold"),
             }),
             gas_limit: None,
             reply_on: ReplyOn::Never,
         };
-        
-        let res = execute(deps.as_mut(), mock_env(), mock_info("sender", &coins(2,"token")), msg);
+
+        let res = execute(
+            deps.as_mut(),
+            mock_env(),
+            mock_info("sender", &coins(2, "token")),
+            msg,
+        );
         assert_eq!(sub, res.unwrap().messages[0]);
     }
 
     #[test]
-    fn amount_zero_test(){
-        let mut deps = mock_dependencies_with_balance(&vec![coin(123, "gold"), coin(456, "silver"), coin(789, "bronze")]);
+    fn amount_zero_test() {
+        let mut deps = mock_dependencies_with_balance(&vec![
+            coin(123, "gold"),
+            coin(456, "silver"),
+            coin(789, "bronze"),
+        ]);
         let chain_name = String::from("chain name");
         let header = String::from("abc");
         let env = mock_env();
 
-        let info = mock_info("sender", &coins(2,"token"));
-        let msg = InstantiateMsg{header, chain_name};
+        let info = mock_info("sender", &coins(2, "token"));
+        let msg = InstantiateMsg { header, chain_name };
         let _res = instantiate(deps.as_mut(), env, info, msg);
         let ftt = FungibleTokenTransfer {
             token_id: String::from("gold"),
@@ -257,9 +305,22 @@ mod test {
             contract_sequence: u64::from(1u64),
         };
 
-        let msg = ExecuteMsg::Transfer { recipient: String::from("recipient"), amount: Uint128::from(0u128), denom: String::from("gold"), message: DeliverableMessage::FungibleTokenTransfer(ftt), block_height: u64::from(00u64), proof: String::from("success") };
-        
-        let err = execute(deps.as_mut(), mock_env(), mock_info("sender", &coins(2,"token")), msg).unwrap_err();
+        let msg = ExecuteMsg::Transfer {
+            recipient: String::from("recipient"),
+            amount: Uint128::from(0u128),
+            denom: String::from("gold"),
+            message: DeliverableMessage::FungibleTokenTransfer(ftt),
+            block_height: u64::from(00u64),
+            proof: String::from("success"),
+        };
+
+        let err = execute(
+            deps.as_mut(),
+            mock_env(),
+            mock_info("sender", &coins(2, "token")),
+            msg,
+        )
+        .unwrap_err();
         match err {
             ContractError::InvalidZeroAmount {} => {}
             e => panic!("unexpected error: {}", e),
@@ -267,14 +328,18 @@ mod test {
     }
 
     #[test]
-    fn verify_fail_test(){
-        let mut deps = mock_dependencies_with_balance(&vec![coin(123, "gold"), coin(456, "silver"), coin(789, "bronze")]);
+    fn verify_fail_test() {
+        let mut deps = mock_dependencies_with_balance(&vec![
+            coin(123, "gold"),
+            coin(456, "silver"),
+            coin(789, "bronze"),
+        ]);
         let chain_name = String::from("chain name");
         let header = String::from("abc");
         let env = mock_env();
 
-        let info = mock_info("sender", &coins(2,"token"));
-        let msg = InstantiateMsg{header, chain_name};
+        let info = mock_info("sender", &coins(2, "token"));
+        let msg = InstantiateMsg { header, chain_name };
         let _res = instantiate(deps.as_mut(), env, info, msg);
         let ftt = FungibleTokenTransfer {
             token_id: String::from("gold"),
@@ -283,9 +348,22 @@ mod test {
             contract_sequence: u64::from(1u64),
         };
 
-        let msg = ExecuteMsg::Transfer { recipient: String::from("recipient"), amount: Uint128::from(10u128), denom: String::from("gold"), message: DeliverableMessage::FungibleTokenTransfer(ftt), block_height: u64::from(10u64), proof: String::from("fail") };
-        
-        let err = execute(deps.as_mut(), mock_env(), mock_info("sender", &coins(2,"token")), msg).unwrap_err();
+        let msg = ExecuteMsg::Transfer {
+            recipient: String::from("recipient"),
+            amount: Uint128::from(10u128),
+            denom: String::from("gold"),
+            message: DeliverableMessage::FungibleTokenTransfer(ftt),
+            block_height: u64::from(10u64),
+            proof: String::from("fail"),
+        };
+
+        let err = execute(
+            deps.as_mut(),
+            mock_env(),
+            mock_info("sender", &coins(2, "token")),
+            msg,
+        )
+        .unwrap_err();
         match err {
             ContractError::VerifyFail {} => {}
             e => panic!("unexpected error: {}", e),
--- contracts/treasury/src/msg.rs
+++ contracts/treasury/src/msg.rs
@@ -1,8 +1,8 @@
+use cosmwasm_std::Uint128;
 use pdao_beacon_chain_common::message::DeliverableMessage;
 use pdao_colony_contract_common::light_client::{BlockFinalizationProof, Header};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
-use cosmwasm_std::{Uint128};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
@@ -25,19 +25,16 @@ pub enum ExecuteMsg {
         message: DeliverableMessage,
         block_height: u64,
         proof: String,
-    }
+    },
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
     GetHeader {},
-    GetBalance {
-        denom: String,
-    },
+    GetBalance { denom: String },
 
-    GetAllBalances {
-    },
+    GetAllBalances {},
 }
 
 // We define a custom struct for each query response
